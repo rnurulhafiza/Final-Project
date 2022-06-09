32db288fdcb3354567d5ddc69a0efb304d9ab64b
rnurulhafiza <107098919+rnurulhafiza@users.noreply.github.com>
2022-06-09 12:46:28 +0800

    Update rajaproject.py

--- rajaproject.py
+++ rajaproject.py
@@ -42,9 +42,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(pd.DataFrame({
-  ['0', '1', '2'] : ['setosa', 'versicolor', 'virginica']
-}))
+st.write(pd.DataFrame({['0', '1', '2'] : ['setosa', 'versicolor', 'virginica']}))
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
