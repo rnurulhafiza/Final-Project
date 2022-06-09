64d1771383a0b44775dd8e4f1216ec89f8a34070
rnurulhafiza <107098919+rnurulhafiza@users.noreply.github.com>
2022-06-09 13:55:29 +0800

    Update rajaproject.py

--- rajaproject.py
+++ rajaproject.py
@@ -42,7 +42,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(['setosa', 'versicolor', 'virginica'])
+st.table(['setosa', 'versicolor', 'virginica'])
 
 
 st.subheader('Prediction')
